224d2adff028f02dd3addc6ce57abebdf55af9c0
Lane Winham <mlwinham12@gmail.com>
2016-06-23 13:57:20 -0600

    I3: number guessing game complete and has some color

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,39 @@
-require "sinatra"
+require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(100)
+SECRET_NUMBER = rand(100)
 
-get "/" do
-  erb :index, :locals => {:number => secret_number}
+get '/' do
+  guess = params["guess"].to_i
+  message = guess_review(guess)
+  color = color_selector(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :output => message, :input_guess => guess, :color => color}
+end
+
+def guess_review(guess)
+  if guess > SECRET_NUMBER + 10
+    message = "Way too high!"
+  elsif guess > SECRET_NUMBER
+    message = "Slightly too high!"
+  elsif guess < SECRET_NUMBER - 10
+    message = "Way too low..."
+  elsif guess < SECRET_NUMBER
+    message = "Slightly too low..."
+  else
+    message = "The secret number is #{SECRET_NUMBER}. You got it right!"
+  end
+end
+
+def color_selector(guess)
+  if guess > SECRET_NUMBER + 10
+    color = "red"
+  elsif guess > SECRET_NUMBER
+    color = "Tomato"
+  elsif guess < SECRET_NUMBER - 10
+    color = "red"
+  elsif guess < SECRET_NUMBER
+    color = "Tomato"
+  else
+    color = "SeaGreen"
+  end
 end
